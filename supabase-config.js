7ff15a4a72d4d7fcfc127fbbcb117c6956d7db9d
James Ge <gelingting@gmail.com>
2026-07-02 22:39:43 -0700

    Configure Supabase project

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.FITNESS_SUPABASE_CONFIG = {
-  url: "",
-  anonKey: ""
+  url: "https://wecjksebhjjqllvvqryp.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndlY2prc2ViaGpqcWxsdnZxcnlwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMwNDM1MzksImV4cCI6MjA5ODYxOTUzOX0.z5u1gh8QQbn60l-VcpHdQa9Ay02EjWAfDKLfN7zM8IM"
 };
